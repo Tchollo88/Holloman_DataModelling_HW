676a77fd2d80834e58566bf2ff396f6e6d7fed38
Tchollo88 <Tcholloman@gmail.com>
2024-10-29 09:39:04 -0700

    Add project files.

---
+++ Holloman_DataModelling_HW/Data/DataModellingDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Holloman_DataModelling_HW.Models;
+
+namespace Holloman_DataModelling_HW.Data
+{
+    public class DataModellingDb : DbContext
+    {
+        public DataModellingDb (DbContextOptions<DataModellingDb> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Holloman_DataModelling_HW.Models.Customer> Customer { get; set; } = default!;
+    }
+}
---
+++ Holloman_DataModelling_HW/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace Holloman_DataModelling_HW.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        /*Phone Number M -> O*/
+        /*Address O -> M*/
+        public string Email { get; set; }
+        /*Likes M -> O*/
+    }
+}
